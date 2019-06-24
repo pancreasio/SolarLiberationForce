78bd705dc51ff6dda5c6e3409cefb7deca743a17
pancreasio <julianholisu@gmail.com>
2019-06-24 12:47:37 -0300

    Player Controller Started
    fuck off slackers

---
+++ Solar Liberation Force/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rig;
+    public float speed;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(rig.velocity);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3 (transform.position.x + Input.GetAxisRaw("Horizontal") * speed,
+                                          transform.position.y,
+                                          transform.position.z + Input.GetAxisRaw("Vertical") * speed);
+    }
+}
